e2473230c8e7661e8339ae56c77afb0200880651
Tianqi Chen <tchen012@uottawa.ca>
2019-10-20 20:34:34 -0400

    final version of Welcome class

--- Welcome.java
+++ Welcome.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     private TextView welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_welcome);
 
         welcome= (TextView)findViewById(R.id.tv_welcome);
     }
