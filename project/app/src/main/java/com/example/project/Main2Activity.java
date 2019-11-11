fbde1fa628136515040ce56e8714bad3134c3a4e
Ryceccww <55290691+Ryceccww@users.noreply.github.com>
2019-11-10 20:40:45 -0500

    Update Main2Activity.java

--- project/app/src/main/java/com/example/project/Main2Activity.java
+++ project/app/src/main/java/com/example/project/Main2Activity.java
@@ -6,16 +6,24 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
 
-import org.w3c.dom.Text;
+
 
 public class Main2Activity extends AppCompatActivity {
+    private TextView Username;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Username = (TextView)findViewById(R.id.textView6);
+
+        Intent intent = getIntent();
+        String name2 = intent.getStringExtra("username");
+        Username.setText(name2);
 
     }
 }
