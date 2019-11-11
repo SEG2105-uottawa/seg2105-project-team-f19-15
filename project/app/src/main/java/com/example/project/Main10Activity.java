f66e6449f5cc2eb0d79734a6b34fb7c7624b7cd5
Ryceccww <55290691+Ryceccww@users.noreply.github.com>
2019-11-10 20:39:54 -0500

    Update Main10Activity.java

--- project/app/src/main/java/com/example/project/Main10Activity.java
+++ project/app/src/main/java/com/example/project/Main10Activity.java
@@ -2,13 +2,25 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main10Activity extends AppCompatActivity {
+    private TextView Username;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main10);
+
+        Username = (TextView)findViewById(R.id.textView3);
+
+        Intent intent = getIntent();
+        String name2 = intent.getStringExtra("username");
+        Username.setText(name2);
+
+
     }
 }
