fe2ffd67441d7fec88eeecd9a9b13ab31318d446
Panzq797 <55156468+Panzq797@users.noreply.github.com>
2019-11-20 09:04:03 -0500

    Update Main10Activity.java

--- project/app/src/main/java/com/example/project/Main10Activity.java
+++ project/app/src/main/java/com/example/project/Main10Activity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 public class Main10Activity extends AppCompatActivity {
     private TextView Username;
+    private Button complete;
 
 
     @Override
@@ -16,10 +17,20 @@ public class Main10Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main10);
 
         Username = (TextView)findViewById(R.id.textView3);
+        complete=(Button)findViewById(R.id.button6) ;
 
         Intent intent = getIntent();
         String name2 = intent.getStringExtra("username");
         Username.setText(name2);
+        complete.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                startActivity(new Intent(Main10Activity.this, Main11Activity.class));
+                Intent intent = new Intent(getApplicationContext(),Main11Activity.class);
+                intent.putExtra("username",Username.getText().toString());
+                startActivity(intent);
+            }
+        });
 
 
     }
