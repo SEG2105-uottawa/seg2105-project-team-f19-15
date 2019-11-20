ab5397df0c00daf17b99965eb25af795b3298f96
Zhihao Yin <zyin043@uottawa.ca>
2019-11-19 22:40:44 -0500

    upgrade

--- project/app/src/test/java/com/example/project/ChooseTimeTest.java
+++ project/app/src/test/java/com/example/project/ChooseTimeTest.java
@@ -1,5 +1,32 @@
 package com.example.project;
 
-public class ChooseTime {
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ChooseTimeTest {
+    private ChooseTime chooseTimeTest = new ChooseTime(7,20,10,30);
+
+    @Test
+    public void isInitialized(){
+        assertNotNull(chooseTimeTest);
+    }
+
+    public  void isCorrectStartingHour(){
+        assertNotNull("10",String.valueOf(chooseTimeTest.getStartingHours()));
+    }
+
+    public void isCorrectStartingMinute(){
+        assertNotNull("30",String.valueOf(chooseTimeTest.getStartingMinutes()));
+    }
+    public void isCorrectEndingHour(){
+        assertNotNull("7",String.valueOf(chooseTimeTest.getEndingHours()));
+    }
+    public  void isCorrectEndingMinute(){
+        assertNotNull("20",String.valueOf(chooseTimeTest.getEndingMinutes()));
+    }
+
+
+
 
 }
