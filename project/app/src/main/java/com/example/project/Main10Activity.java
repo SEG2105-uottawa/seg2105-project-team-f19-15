2ca6a89d208011e1ecd6713784d8d2790a6d2e01
Tianqi Chen <tchen012@uottawa.ca>
2019-11-20 19:25:47 -0500

    fix some small problem

--- project/app/src/main/java/com/example/project/Main10Activity.java
+++ project/app/src/main/java/com/example/project/Main10Activity.java
@@ -1,16 +1,21 @@
 package com.example.project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main10Activity extends AppCompatActivity {
-    private TextView Username;
-    private Button complete;
+    private TextView Username, wantTo;
+    private Button complete, seeTime;
+    private MyDBHandler db;
+
 
 
     @Override
@@ -19,11 +24,15 @@ public class Main10Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main10);
 
         Username = (TextView)findViewById(R.id.textView3);
-        complete=(Button)findViewById(R.id.button6) ;
+        complete=(Button)findViewById(R.id.button6);
+        seeTime= (Button)findViewById(R.id.btnSeeTime);
+        wantTo= (TextView)findViewById(R.id.tvWant);
+        db= new MyDBHandler(this);
 
         Intent intent = getIntent();
         String name2 = intent.getStringExtra("username");
         Username.setText(name2);
+
         complete.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View view){
@@ -34,6 +43,33 @@ public class Main10Activity extends AppCompatActivity {
             }
         });
 
+        seeTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor cursor= db.viewAll();
+                if(cursor.getCount()== 0){
+                    Toast.makeText(getApplicationContext(), "No available time to show at this time.", Toast.LENGTH_SHORT).show();
+                }else{
+                    StringBuffer buffer= new StringBuffer();
+                    while(cursor.moveToNext()){
+                        buffer.append("StartingHours:"+ cursor.getString(0)+ "\n");
+                        buffer.append("StartMinutes: "+ cursor.getString(1)+ "\n");
+                        buffer.append("EndingHours: "+ cursor.getString(2)+ "\n");
+                        buffer.append("EndingMinutes: "+ cursor.getString(3)+ "\n\n");
+                    }
 
+                    show("Available time", buffer.toString());
+                }
+            }
+        });
+
+
+    }
+    private void show(String title, String message){
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
