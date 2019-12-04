f785e466ae2910dc671bf532d4d451075963328e
Tianqi Chen <tchen012@uottawa.ca>
2019-12-03 22:35:30 -0500

    Update ver of Main Search

--- project/app/src/main/java/com/example/project/MainSearchClinicActivity.java
+++ project/app/src/main/java/com/example/project/MainSearchClinicActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainSearchClinicActivity extends AppCompatActivity {
-    private EditText address, workingHour, serviceType;
+    private EditText address, startHr, endHr, startMin, endMin, serviceType;
     private Button see;
     private MyDBHandler db;
 
@@ -21,7 +21,10 @@ public class MainSearchClinicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_search_clinic);
 
         address= (EditText) findViewById(R.id.etAddress);
-        workingHour= (EditText) findViewById(R.id.etworkingHour);
+        startHr= (EditText) findViewById(R.id.etstartingHour);
+        endHr= (EditText) findViewById(R.id.etendingHour);
+        startMin= (EditText) findViewById(R.id.etstartingMin);
+        endMin= (EditText) findViewById(R.id.etendingMin);
         serviceType= (EditText) findViewById(R.id.etserviceType);
         see= (Button) findViewById(R.id.btnSee);
         db= new MyDBHandler(this);
@@ -30,10 +33,13 @@ public class MainSearchClinicActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String addr= address.getText().toString();
-                String wh= workingHour.getText().toString();
+                String startWorkingHr= startHr.getText().toString();
+                String startWorkingMin= startMin.getText().toString();
+                String endWorkingHr= endHr.getText().toString();
+                String endWorkingMin= endMin.getText().toString();
                 String st= serviceType.getText().toString();
 
-                if(addr!= null && wh== null && st== null){
+                if(addr!= null && (startWorkingHr== null && startWorkingMin== null && endWorkingHr== null && endWorkingMin== null) && st== null){
                     boolean findClinic= db.findClinicByAddress(addr);
                     if(findClinic){
                         Toast.makeText(getApplicationContext(), "This clinic is found by the address you entered. ", Toast.LENGTH_SHORT).show();
@@ -43,10 +49,25 @@ public class MainSearchClinicActivity extends AppCompatActivity {
                     }
 
 
-           //     }else if(addr== null && wh!= null && st== null){
-          //          boolean findClinic= db.findClinicByWorkingHour(wh);
-           //     }else if(addr== null && wh== null && st!= null){
-           //         boolean findClinic= db.findClinicByServiceType(st);
+                }else if(addr== null && (startWorkingHr!= null && startWorkingMin!= null && endWorkingHr!= null && endWorkingMin!= null) && st== null){
+                    boolean findClinic= db.findClinicByWorkingHour(startWorkingHr, startWorkingMin, endWorkingHr, endWorkingMin);
+                    if(findClinic){
+                        Toast.makeText(getApplicationContext(), "This clinic is found by the working hour you entered. ", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainSearchClinicActivity.this, MainBookCheckActivity.class));
+                    }else{
+                        Toast.makeText(getApplicationContext(), "There is no clinic who has the working hour you entered. ", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }else if(addr== null && (startWorkingHr== null && startWorkingMin== null && endWorkingHr== null && endWorkingMin== null) && st!= null){
+                    boolean findClinic= db.findClinicByServiceType(st);
+                    if(findClinic){
+                        Toast.makeText(getApplicationContext(), "This clinic is found by the type of service you entered. ", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainSearchClinicActivity.this, MainBookCheckActivity.class));
+                    }else{
+                        Toast.makeText(getApplicationContext(), "There is no clinic who has the type of service you entered. ", Toast.LENGTH_SHORT).show();
+                    }
+
                 }else{
                     Toast.makeText(getApplicationContext(), "You are only allowed to fill one of three blanks! ", Toast.LENGTH_SHORT).show();
                 }
